848e3c75c8353485caaec8ef11715cb8f506ef05
ChristopherDancy <christopher.dancy@pega.com>
2016-02-28 11:18:57 -0500

    ADDED: when specifying a directory of artifacts to deploy we will now search recursively for all regular files therein

--- src/main/groovy/com/github/gradle/artifactory/rest/tasks/artifact/DeployArtifact.groovy
+++ src/main/groovy/com/github/gradle/artifactory/rest/tasks/artifact/DeployArtifact.groovy
@@ -16,6 +16,7 @@
 package com.github.gradle.artifactory.rest.tasks.artifact
 
 import com.github.gradle.artifactory.rest.tasks.AbstractArtifactoryRestTask
+import groovy.io.FileType
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
@@ -49,18 +50,25 @@ class DeployArtifact extends AbstractArtifactoryRestTask {
             def api = artifactoryClient.api().artifactApi()
 
             if (file && file.exists() && !file.isDirectory()) {
+
                 def payload = threadContextClassLoader.newPayload(file)
                 deployedArtifact = api.deployArtifact(tempRepo, tempArtifactPath, payload)
                 logger.quiet "Artifact successfully deployed @ ${deployedArtifact.repo}:${deployedArtifact.path}"
             } else if (directory && directory.exists() && directory.isDirectory()) {
-                deployedArtifact = []
-                for (File it : directory.listFiles()) {
-                    if (!it.isDirectory()) {
+
+                def fileList = []
+                directory.eachFileRecurse (FileType.FILES) { fileList << it }
+                if (fileList) {
+                    deployedArtifact = [] // set deployedArtifact to be a List as we have potentially N number of files
+                    for (File it : fileList) {
                         def payload = threadContextClassLoader.newPayload(it)
-                        def possibleArtifact = api.deployArtifact(tempRepo, tempArtifactPath + "/${it.name}", payload)
+                        String newBasePath = it.path.replaceFirst(directory.path, "").replaceFirst("/", "")
+                        def possibleArtifact = api.deployArtifact(tempRepo, tempArtifactPath + "/${newBasePath}", payload)
                         deployedArtifact.add(possibleArtifact)
                         logger.quiet "Artifact successfully deployed @ ${possibleArtifact.repo}:${possibleArtifact.path}"
                     }
+                } else {
+                    throw new GradleException("Could not find any regular files under directory @ ${directory.path}")
                 }
             } else {
                 throw new GradleException("`file` and/or `directory` are not valid")
